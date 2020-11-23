84e13c0f2c502e76237d268fb7d8a3aba3c5ab3a
obidkon <obid.sharipov.80@mail.ru>
2020-11-23 21:11:22 +0500

    bu math

---
+++ src/Math.java
@@ -0,0 +1,10 @@
+public class Math {
+    public static void main(String[] args) {
+        //int piyola ,choynak , idishlar;
+       double piyola ,choynak , idishlar;
+        piyola=16;
+        choynak=7;
+        idishlar=piyola%choynak;
+        System.out.println(idishlar);
+    }
+}
